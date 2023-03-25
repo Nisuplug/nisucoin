7b61ce5a1b73fad6758485f8dd54d980273d4f3c
Nisuplug <128902055+Nisuplug@users.noreply.github.com>
2023-03-25 14:13:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let button = document.querySelector("#coin");
+button.addEventListener("mouseup", (e) => {
+    let addBalance = balance.innerText++;
+    switch (e.button) {
+        case 0:
+            addBalance;
+    }
+});
